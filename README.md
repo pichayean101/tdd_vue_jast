9689f5cf702fbb49180af083b9fed6eb51a93740
pichayean101 <pichayeanyensiri@gmail.com>
2018-08-22 21:58:20 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# vue-test-utils-jest-example
+
+> An example project using vue-test-utils and jest to run unit tests
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+# reference 01
+https://github.com/vuejs/vue-test-utils-jest-example
+
+# reference 02
+https://medium.freecodecamp.org/simple-unit-tests-with-vue-test-utils-and-jest-c384d7abc321
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/_tests_/MessageToggle.spec.js
@@ -0,0 +1,36 @@
+// import Vue from 'vue'
+// import MessageToggle from '@/components/MessageToggle.vue'
+
+// describe('MessageToggle.vue', () => {
+//   it('displays default message', () => {
+//     const Ctor = Vue.extend(MessageToggle)
+//     const vm = new Ctor().$mount()
+
+//     expect(vm.$el.textContent).toContain('default message')
+//   })
+// })
+
+
+import { mount } from 'vue-test-utils'
+import MessageToggle from '@/components/MessageToggle.vue'
+
+describe('MessageToggle.vue', () => {
+  it('displays default message', () => {
+    const wrapper = mount(MessageToggle)
+    expect(wrapper.text()).toContain('default message')
+  })
+
+  it('toggles message when button is clicked', () => {
+    const wrapper = mount(MessageToggle)
+    const button = wrapper.find('#toggle-message')
+    const p = wrapper.find('p')
+
+    button.trigger('click')
+
+    expect(p.text()).toBe('message')
+
+    button.trigger('click')
+
+    expect(p.text()).toBe('toggled message')
+  })
+})
